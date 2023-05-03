53f1c7744bf1dbfe7be8242c65de822a3d9d6a82
Дмитро <dmitry.bilyk.1990@gmail.com>
2023-05-03 23:20:09 +0300

    8. SignUp, SignIn, Main done

--- app/src/main/java/com/example/fibasketfood/SignUpActivity.java
+++ app/src/main/java/com/example/fibasketfood/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.fibasketfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fibasketfood.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -25,6 +34,37 @@ public class SignUpActivity extends AppCompatActivity {
         edtTxtPassword = findViewById(R.id.edtTxtPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
         txtSignIn = findViewById(R.id.txtSignIn);       // android:onClick="SignInA"
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();     //init firebase
+        final DatabaseReference table_user = database.getReference("User");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.child(edtTxtLogin.getText().toString()).exists()) {        //перевіряємо чи існує користувач в базі
+                            Toast.makeText(SignUpActivity.this, "Already registered", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            User user = new User(edtTxtName.getText().toString(), edtTxtPassword.getText().toString());
+                            table_user.child(edtTxtLogin.getText().toString()).setValue(user);
+                            Toast.makeText(SignUpActivity.this, "Sign up successfully", Toast.LENGTH_LONG).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
     }
 
     public void SignInA(View view) {
