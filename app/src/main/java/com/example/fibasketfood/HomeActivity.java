f4aa93acc451d8029dc7c1f0442c25f68b2755c2
dmitry.bilyk.1990@gmail.com <yashikmail1990>
2023-05-18 16:15:34 +0300

    12 Home - Category Recycler View from Firebase

--- app/src/main/java/com/example/fibasketfood/HomeActivity.java
+++ app/src/main/java/com/example/fibasketfood/HomeActivity.java
@@ -1,14 +1,44 @@
 package com.example.fibasketfood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.fibasketfood.Model.Category;
+import com.example.fibasketfood.ViewHolder.MenuViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class HomeActivity extends AppCompatActivity {
+
+    RecyclerView recyclerMenu;
+    FirebaseRecyclerOptions<Category> options;
+    MenuViewHolder menuViewHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerMenu = findViewById(R.id.recyclerMenu);
+        recyclerMenu.setLayoutManager(new LinearLayoutManager(this));
+
+        options = new FirebaseRecyclerOptions.Builder<Category>().setQuery(FirebaseDatabase.getInstance().getReference().child("category"), Category.class).build();
+        menuViewHolder = new MenuViewHolder(options);
+        recyclerMenu.setAdapter(menuViewHolder);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        menuViewHolder.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        menuViewHolder.stopListening();
     }
 }
---
+++ app/src/main/java/com/example/fibasketfood/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,57 @@
+package com.example.fibasketfood.ViewHolder;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fibasketfood.Model.Category;
+import com.example.fibasketfood.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.squareup.picasso.Picasso;
+
+public class MenuViewHolder extends FirebaseRecyclerAdapter<Category, MenuViewHolder.mHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public MenuViewHolder(@NonNull FirebaseRecyclerOptions<Category> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull mHolder holder, int position, @NonNull Category model) {
+        holder.txtMenuName.setText(model.getName());
+        Picasso.get().load(model.getImage())
+                .into(holder.imgView);
+    }
+
+    @NonNull
+    @Override
+    public mHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_item, parent, false);
+        return new mHolder(view);
+    }
+
+    class mHolder extends RecyclerView.ViewHolder {
+
+        public TextView txtMenuName;
+        public ImageView imgView;
+
+
+        public mHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtMenuName = itemView.findViewById(R.id.txtMenuName);
+            imgView = itemView.findViewById(R.id.imgView);
+        }
+    }
+}
