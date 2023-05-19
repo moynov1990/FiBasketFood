6df2998f283a2ca0833cb0031c78776c82463c75
dmitry.bilyk.1990@gmail.com <yashikmail1990>
2023-05-19 16:54:11 +0300

    13 Home - Side Menu

--- app/src/main/java/com/example/fibasketfood/HomeActivity.java
+++ app/src/main/java/com/example/fibasketfood/HomeActivity.java
@@ -1,10 +1,16 @@
 package com.example.fibasketfood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.fibasketfood.Model.Category;
 import com.example.fibasketfood.ViewHolder.MenuViewHolder;
@@ -16,6 +22,8 @@ public class HomeActivity extends AppCompatActivity {
     RecyclerView recyclerMenu;
     FirebaseRecyclerOptions<Category> options;
     MenuViewHolder menuViewHolder;
+    DrawerLayout drawerLayout;
+    ImageView imgMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,16 @@ public class HomeActivity extends AppCompatActivity {
         options = new FirebaseRecyclerOptions.Builder<Category>().setQuery(FirebaseDatabase.getInstance().getReference().child("category"), Category.class).build();
         menuViewHolder = new MenuViewHolder(options);
         recyclerMenu.setAdapter(menuViewHolder);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        imgMenu = findViewById(R.id.imgMenu);
+
+        imgMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     @Override
