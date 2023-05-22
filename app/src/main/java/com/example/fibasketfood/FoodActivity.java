007f5ee8e57bf0c9ec6e23bb1caee20b4e0d9e61
dmitry.bilyk.1990@gmail.com <yashikmail1990>
2023-05-22 17:24:44 +0300

    15 Food Activity

--- app/src/main/java/com/example/fibasketfood/FoodActivity.java
+++ app/src/main/java/com/example/fibasketfood/FoodActivity.java
@@ -1,14 +1,39 @@
 package com.example.fibasketfood;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.fibasketfood.Model.Category;
+import com.example.fibasketfood.Model.Food;
+import com.example.fibasketfood.ViewHolder.FoodViewHolder;
+import com.example.fibasketfood.ViewHolder.MenuViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FoodActivity extends AppCompatActivity {
+
+    RecyclerView recyclerFood;
+    ConstraintLayout foodLayout;
+    FirebaseRecyclerOptions<Food> fOptions;
+    FoodViewHolder foodViewHolder;
+    ImageView imgFoodView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        recyclerFood = findViewById(R.id.recyclerFood);
+        recyclerFood.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/fibasketfood/ViewHolder/FoodViewHolder.java
+++ app/src/main/java/com/example/fibasketfood/ViewHolder/FoodViewHolder.java
@@ -1,5 +1,58 @@
 package com.example.fibasketfood.ViewHolder;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class FoodViewHolder {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fibasketfood.Model.Category;
+import com.example.fibasketfood.Model.Food;
+import com.example.fibasketfood.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.squareup.picasso.Picasso;
+
+public class FoodViewHolder extends FirebaseRecyclerAdapter<Food, FoodViewHolder.fHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public FoodViewHolder(@NonNull FirebaseRecyclerOptions<Food> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull FoodViewHolder.fHolder holder, int position, @NonNull Food model) {
+        holder.txtFoodName.setText(model.getName());
+        Picasso.get().load(model.getImage())
+                .into(holder.imgFoodView);
+    }
+
+    @NonNull
+    @Override
+    public FoodViewHolder.fHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_item, parent, false);
+        return new fHolder(view);
+    }
+
+    class fHolder extends RecyclerView.ViewHolder {
+
+        public TextView txtFoodName;
+        public ImageView imgFoodView;
+
+
+        public fHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtFoodName = itemView.findViewById(R.id.txtFoodName);
+            imgFoodView = itemView.findViewById(R.id.imgFoodView);
+        }
+    }
 }
