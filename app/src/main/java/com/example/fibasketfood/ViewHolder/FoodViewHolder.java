e6202e3f8528c07618c226b233d83a6ace68a1b6
dmitry.bilyk.1990@gmail.com <yashikmail1990>
2023-06-05 16:58:28 +0300

    18 food_item.xml

--- app/src/main/java/com/example/fibasketfood/ViewHolder/FoodViewHolder.java
+++ app/src/main/java/com/example/fibasketfood/ViewHolder/FoodViewHolder.java
@@ -9,12 +9,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fibasketfood.Database.Database;
 import com.example.fibasketfood.Model.Category;
 import com.example.fibasketfood.Model.Food;
+import com.example.fibasketfood.Model.Order;
 import com.example.fibasketfood.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.squareup.picasso.Picasso;
+import android.content.Context;
 
 public class FoodViewHolder extends FirebaseRecyclerAdapter<Food, FoodViewHolder.fHolder> {
 
@@ -45,7 +48,7 @@ public class FoodViewHolder extends FirebaseRecyclerAdapter<Food, FoodViewHolder
     class fHolder extends RecyclerView.ViewHolder {
 
         public TextView txtFoodName;
-        public ImageView imgFoodView;
+        public ImageView imgFoodView, imgAddToBasket;
 
 
         public fHolder(@NonNull View itemView) {
@@ -54,6 +57,14 @@ public class FoodViewHolder extends FirebaseRecyclerAdapter<Food, FoodViewHolder
             txtFoodName = itemView.findViewById(R.id.txtFoodName);
             imgFoodView = itemView.findViewById(R.id.imgFoodView);
 
+            imgAddToBasket = itemView.findViewById(R.id.imgAddToBasket);
+
+            imgAddToBasket.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
         }
     }
 }
